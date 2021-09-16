f8ac080ae2bfcf78252c62629320838f6a341379
Joao <joaogcfa@al.insper.edu.br>
2021-09-16 16:42:50 -0300

    criando CPU e outras coisas

--- Aula7/Aula7.vhd
+++ Aula7/Aula7.vhd
@@ -12,12 +12,12 @@ entity Aula7 is
   );
   port   (
     CLOCK_50 : in std_logic;
-    KEY: in std_logic_vector(3 downto 0);
-	 BARRAMENTO_DADOS_SAIDA: out std_logic_vector(larguraDados-1 downto 0);
-	 BARRAMENTO_DADOS_ENTRADA: out std_logic_vector(larguraDados-1 downto 0);
-	 BARRAMENTO_DADOS_ENDERECOS: out std_logic_vector(8 downto 0)
-    --SW: in std_logic_vector(9 downto 0);
-    --LEDR  : out std_logic_vector(9 downto 0)
+    KEY: in std_logic_vector(3 downto 0)
+	 --BARRAMENTO_DADOS_SAIDA: out std_logic_vector(larguraDados-1 downto 0);
+	 --BARRAMENTO_DADOS_ENTRADA: out std_logic_vector(larguraDados-1 downto 0);
+	 --BARRAMENTO_DADOS_ENDERECOS: out std_logic_vector(8 downto 0)
+--    SW: in std_logic_vector(9 downto 0);
+--    LEDR  : out std_logic_vector(9 downto 0)
   );
 end entity;
 
@@ -25,33 +25,33 @@ end entity;
 architecture arquitetura of Aula7 is
 
 -- Faltam alguns sinais:
-  signal MUX_ULA_B : std_logic_vector (larguraDados-1 downto 0);
-  signal MUX_REG1 : std_logic_vector (larguraDados-1 downto 0);
-  signal REG1_ULA_A : std_logic_vector (larguraDados-1 downto 0);
-  signal Saida_ULA : std_logic_vector (larguraDados-1 downto 0);
-  signal Sinais_Controle : std_logic_vector (3 downto 0);
-  --signal Proximo_Estado : std_logic_vector (3 downto 0);
-  --signal Estado_Atual : std_logic_vector (3 downto 0);
-  signal Chave_Operacao_ULA : std_logic;
-  signal CLK : std_logic;
-  signal SelMUX : std_logic;
-  signal Habilita_A : std_logic;
-  signal Operacao_ULA : std_logic_vector (1 downto 0);
-  signal Saida_Dados : std_logic_vector (larguraDados-1 downto 0);
-  signal Imediato : std_logic_vector (larguraDados-1 downto 0);
-  signal opCode : std_logic_vector (3 downto 0);
-  signal endereco_PC:std_logic_vector (8 downto 0);
-  signal endereco :std_logic_vector (8 downto 0);
-  signal habLeituraMEM : std_logic;
-  signal habEscritaMEM : std_logic;
-  signal Saida_Somador : std_logic_vector (8 downto 0); 
-  signal Mux_PC : std_logic_vector (8 downto 0); 
-  signal Endereco_Imediato : std_logic_vector (8 downto 0);
-  signal Saida_Decod : STD_LOGIC_VECTOR(11 downto 0);
-  signal Saida_FlipFlop : std_logic;
-  signal Saida_ULA_Flag0 : std_logic;
-  signal Saida_LogicaDesvio : std_logic_vector (1 downto 0);
-  signal SaidaReg_MUX_C : std_logic_vector (8 downto 0);
+--  signal MUX_ULA_B : std_logic_vector (larguraDados-1 downto 0);
+--  signal MUX_REG1 : std_logic_vector (larguraDados-1 downto 0);
+--  signal REG1_ULA_A : std_logic_vector (larguraDados-1 downto 0);
+--  signal Saida_ULA : std_logic_vector (larguraDados-1 downto 0);
+--  signal Sinais_Controle : std_logic_vector (3 downto 0);
+--  --signal Proximo_Estado : std_logic_vector (3 downto 0);
+--  --signal Estado_Atual : std_logic_vector (3 downto 0);
+--  signal Chave_Operacao_ULA : std_logic;
+    signal CLK : std_logic;
+--  signal SelMUX : std_logic;
+--  signal Habilita_A : std_logic;
+--  signal Operacao_ULA : std_logic_vector (1 downto 0);
+--  signal Saida_Dados : std_logic_vector (larguraDados-1 downto 0);
+--  signal Imediato : std_logic_vector (larguraDados-1 downto 0);
+--  signal opCode : std_logic_vector (3 downto 0);
+--  signal endereco_PC:std_logic_vector (8 downto 0);
+--  signal endereco :std_logic_vector (8 downto 0);
+--  signal habLeituraMEM : std_logic;
+--  signal habEscritaMEM : std_logic;
+--  signal Saida_Somador : std_logic_vector (8 downto 0); 
+--  signal Mux_PC : std_logic_vector (8 downto 0); 
+--  signal Endereco_Imediato : std_logic_vector (8 downto 0);
+--  signal Saida_Decod : STD_LOGIC_VECTOR(11 downto 0);
+--  signal Saida_FlipFlop : std_logic;
+--  signal Saida_ULA_Flag0 : std_logic;
+--  signal Saida_LogicaDesvio : std_logic_vector (1 downto 0);
+--  signal SaidaReg_MUX_C : std_logic_vector (8 downto 0);
 
 begin
 
@@ -66,59 +66,23 @@ detectorSub0: work.edgeDetector(bordaSubida)
 end generate;
 
 -- O port map completo do MUX.
-MUX1 :  entity work.muxGenerico2x1  generic map (larguraDados => larguraDados)
-        port map( entradaA_MUX => Saida_Dados,
-                 entradaB_MUX =>  Endereco_Imediato(7 downto 0),
-                 seletor_MUX => Saida_Decod(6),
-                 saida_MUX => MUX_ULA_B);
-					  
-DECODIFICADOR_INSTRUCAO : entity work.DecodInstrucao -- apagamos o generic map
-          port map (CodigoBinario => opCode, Saida => Saida_Decod);
-			 
-			 
--- O port map completo do Acumulador.
-PC : entity work.registradorGenerico_PC   generic map (larguraDados => larguraDados_PC)
-          port map (DIN => Mux_PC, DOUT => endereco_PC, ENABLE => '1', RST => '0', CLK => CLK);
-			 
-SOMADOR  :  entity work.somaConstante  generic map (larguraDados => larguraDados_PC, constante => 1) ----colocamos 1 na constante
-        port map( entrada => endereco_PC, saida => Saida_Somador);
+CPU : entity work.CPU
+port map   (
+    CLK => CLK,
+    INTRUCTION_IN => 
+	 RD: out std_logic;
+	 WR: out std_logic;
+	 BARRAMENTO_DADOS_SAIDA: out std_logic_vector(larguraDados-1 downto 0);
+	 BARRAMENTO_DADOS_ENTRADA: out std_logic_vector(larguraDados-1 downto 0);
+	 BARRAMENTO_DADOS_ENDERECOS: out std_logic_vector(8 downto 0)
+  );
 
-REG_END_RET : entity work.registradorGenerico_PC   generic map (larguraDados => larguraDados_PC)
-          port map (DIN => Saida_Somador, DOUT => SaidaReg_MUX_C, ENABLE => Saida_Decod(11), RST => '0', CLK => CLK);
-
--- O port map completo do Acumulador.
-REG1 : entity work.registradorGenerico   generic map (larguraDados => larguraDados)
-          port map (DIN => Saida_ULA, DOUT => REG1_ULA_A, ENABLE => Saida_Decod(5), RST => '0',CLK => CLK);
-
-
--- O port map completo da ULA:
-ULA1 : entity work.ULASomaSub  generic map(larguraDados => larguraDados)
-          port map (entradaA => REG1_ULA_A, entradaB => MUX_ULA_B, seletor => Saida_Decod(4 downto 3), saida => Saida_ULA, flag_0 => Saida_ULA_Flag0);
-
--- Falta acertar o conteudo da ROM (no arquivo memoriaROM.vhd)
 MEMORIA_INTRUCAO : entity work.memoriaROM   generic map (dataWidth => larguraInstrucao, addrWidth => larguraEnderecoROM)
           port map (Endereco => Endereco_PC, Dado(8 downto 0) => Endereco_Imediato, Dado(12 downto 9) => opCode);
 
 MEMORIA_DADOS : entity work.memoriaRAM   generic map (dataWidth => larguraDados, addrWidth => larguraEnderecoRAM)
           port map (addr => Endereco_Imediato(7 downto 0), we => Saida_Decod(0), re=>Saida_Decod(1), habilita=>Endereco_Imediato(8), dado_in => REG1_ULA_A, dado_out => Saida_Dados, clk => CLK);		
 
-		
-FLAG : 	entity work.FlipFlop   
-         port map ( DIN => Saida_ULA_Flag0, DOUT => Saida_FlipFlop, ENABLE => Saida_Decod(2), RST => '0',  CLK => CLK);		
-
-			
-LOGICA_DE_DESVIO : entity work.LogicaDesvio   
-         port map ( JMP =>Saida_Decod(10), JEQ =>Saida_Decod(7), Flag =>Saida_FlipFlop, RET =>Saida_Decod(9) , JSR =>Saida_Decod(8) , Saida => Saida_LogicaDesvio );		
-
-		
--- O port map completo do MUX.
-MUX2 :  entity work.muxGenerico2x1_PC  generic map (larguraDados => larguraDados_PC)
-        port map( entradaA_MUX => Saida_Somador,
-                 entradaB_MUX =>  Endereco_Imediato(8 downto 0),
-					  entradaC_MUX => SaidaReg_MUX_C,
-					  entradaD_MUX => "000000000",
-                 seletor_MUX => Saida_LogicaDesvio,
-                 saida_MUX => Mux_PC);
 					  
 BARRAMENTO_DADOS_SAIDA <= REG1_ULA_A;
 BARRAMENTO_DADOS_ENTRADA <= Saida_Dados;
--- Aula7/Decod3x8vhd.vhd
+++ Aula7/Decod3x8vhd.vhd
@@ -1,27 +1,30 @@
-ibrary IEEE;
+library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity Decod3x8 is
 	port (
-			E1 : in  STD_LOGIC;
-			E2 : in  STD_LOGIC;
-			E0 : in  STD_LOGIC;
-			S0 : out STD_LOGIC_VECTOR(4 downto 0));
-			S1 : out STD_LOGIC_VECTOR(4 downto 0));
-			S2 : out STD_LOGIC_VECTOR(4 downto 0));
-			S3 : out STD_LOGIC_VECTOR(4 downto 0));
-			S4 : out STD_LOGIC_VECTOR(4 downto 0));
-			S5 : out STD_LOGIC_VECTOR(4 downto 0));
-			S6 : out STD_LOGIC_VECTOR(4 downto 0));
-			S7 : out STD_LOGIC_VECTOR(4 downto 0));
-			S8 : out STD_LOGIC_VECTOR(4 downto 0))
-			
+			ENTRADA : in STD_LOGIC_VECTOR(2 downto 0);
+			S0 : out STD_LOGIC;
+			S1 : out STD_LOGIC;
+			S2 : out STD_LOGIC;
+			S3 : out STD_LOGIC;
+			S4 : out STD_LOGIC;
+			S5 : out STD_LOGIC;
+			S6 : out STD_LOGIC;
+			S7 : out STD_LOGIC
+	);
 end entity;
 
-architecture arch of DecodInstrucao is
+architecture arch of Decod3x8 is
 begin
 
-	with CodigoBinario (3 downto 0) select
-		
-	
+	S0 <= '1' when ENTRADA = "000" else '0';
+	S1 <= '1' when ENTRADA = "001" else '0';
+	S2 <= '1' when ENTRADA = "010" else '0';
+	S3 <= '1' when ENTRADA = "011" else '0';
+	S4 <= '1' when ENTRADA = "100" else '0';
+	S5 <= '1' when ENTRADA = "101" else '0';
+	S6 <= '1' when ENTRADA = "110" else '0';
+	S7 <= '1' when ENTRADA = "111" else '0';
+			
 end architecture;
